dc6d6a479fe42abfab8c27d33b1489d4cf1254ae
Victor Joaquin Rodriguez <xorjoahec@gmail.com>
2023-08-25 01:16:10 -0300

    :)

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ const fs = require("fs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/",function(req,res){
-    res.sendFile(path.join(__dirname,"./views/home.html"));
+    res.sendFile(path.join(__dirname,"/views/home.html"));
 });
 
 app.listen(3000,()=>{
     console.log("Sucess");
+});
+
+app.get(["/register"], (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
+  });
+app.get(["/login"], (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
 });
